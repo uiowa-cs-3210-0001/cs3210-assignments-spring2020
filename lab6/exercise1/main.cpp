29a3b2e8d13e68ef7384d6875ed7f4d4b3275830
Aleksey Gurtovoy <agurtovoy@acm.org>
2020-02-22 11:01:22 -0600

    Correct lab6/exercise1

--- lab6/exercise1/main.cpp
+++ lab6/exercise1/main.cpp
@@ -1,26 +1,9 @@
 #include <iostream>
-
-int n = -1;
+#include <cassert>
 
 int main() {
-    do
-    {
-        int n = -1;
-        std::cout << "Enter a positive number: ";
-        std::cin >> n;
-    } while ( n < 0 );
-
-    std::cout << "You entered " << ::n << "\n";
-
-    std::cout << "Numbers from zero to " << n - 1 << ":\n";
-    int i = 0;
-    while( i < n )
-        std::cout << i++ << " ";
-
-    std::cout << "\nNumbers from " << n << " to zero:\n";
-    int i = n;
-    while( i >= 0 )
-        std::cout << i-- << " ";
-
-    std::cout << "\n";
+    assert( 17 - 10 / 3 - 1 == 12 );
+    assert( 17 - 10 * 2 > 0 );
+    assert( 17 < 10 || 1 + 1 << 1 == 3 );
+    std::cout << "All tests pass\n";
 }
