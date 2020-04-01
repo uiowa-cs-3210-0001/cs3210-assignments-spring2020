627d5d68ee62b8f4c67ad2f4c81c20f26b4381cc
Aleksey Gurtovoy <agurtovoy@acm.org>
2020-03-31 21:25:29 -0500

    lab16

---
+++ lab16/exercise1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // TODO:
+    // 1. Ask user for a number N
+    // 2. Ask user to enter N integers
+    // 3. Print the space-separated sequence of integers to the console
+    // 
+    // Use functions to raise the level of abstraction of your code
+}
